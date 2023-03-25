505ad4de9dc33691345f9fa5234514a94a6c0f2d
Shahryar K <shahryarkiani04@gmail.com>
2023-03-25 12:40:36 -0400

    Initialize spring project

---
+++ backend/CreditCarbonBackend/src/test/java/com/creditcarbon/creditcarbonbackend/CreditCarbonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditcarbon.creditcarbonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCarbonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
